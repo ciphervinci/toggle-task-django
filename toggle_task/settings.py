47be5625071b21ca8017ea08647b3cf4a0432cc9
Da Vinci <davinci45@protonmail.com>
2021-12-08 19:45:06 +0530

    Added local_settings in local system

--- toggle_task/settings.py
+++ toggle_task/settings.py
@@ -130,3 +130,8 @@ LOGIN_URL = '/login'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from .local_settings import *
+except ImportError:
+    print('Looks like no local file. You are in Production for sure')
